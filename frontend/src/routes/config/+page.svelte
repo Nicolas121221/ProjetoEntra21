2d202dbe18c978170670b1edc8cc26821ed73067
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-06-16 11:27:06 -0300

    feat(frontend): adds a config screen

---
+++ frontend/src/routes/config/+page.svelte
@@ -0,0 +1,26 @@
+<aside class="bg-gray-900/80 fixed left-0 bottom-0 top-0 w-56 border-r border-white/20 text-white z-20 capitalize ">
+<h1 class="text-center text-2xl border-b border-white/20 py-5">Configurações</h1>
+    <section class="flex flex-col items-center">
+        <a href="/config/" class="px-5 py-3 m-1 hover:border hover:border-white/50 rounded-full hover:bg-gray-800 duration-50 w-[95%] text-center active:bg-blue-950">Geral</a>
+        <a href="/config/acessiblidade" class="px-5 py-3 m-1 hover:border hover:border-white/50 rounded-full hover:bg-gray-800 duration-50 w-[95%] text-center active:bg-blue-950">Acessibilidade</a>
+        <a href="/config/interface" class="px-5 py-3 m-1 hover:border hover:border-white/50 rounded-full hover:bg-gray-800 duration-50 w-[95%] text-center active:bg-blue-950">Interface</a>
+        <a href="/config/conta" class="px-5 py-3 m-1 hover:border hover:border-white/50 rounded-full hover:bg-gray-800 duration-50 w-[95%] text-center active:bg-blue-950">Conta</a>
+    </section>
+</aside>
+<main class="bg-gray-800 absolute right-0 left-0 pl-56">
+    <section class="text-white m-10 my-20"> 
+        <h2 class="text-5xl font-semibold">Geral</h2>
+        <p class="inline-block w-2xl mt-3 opacity-80">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus autem, obcaecati possimus molestiae nihil pariatur nostrum inventore sed perferendis qui voluptas quis architecto expedita? Enim odit nostrum esse facilis rerum?</p>
+    </section>
+
+    <section class="h-screen text-white m-10 my-20">
+        <h3 class="text-2xl font-semibold">Temas</h3>
+        <p class="inline-block w-xl mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus autem, obcaecati possimus molestiae nihil pariatur nostrum inventore sed perferendis qui voluptas quis architecto expedita? Enim odit nostrum esse facilis rerum?</p>
+        <h3 class="text-2xl font-semibold mt-5">Alto Contraste</h3>
+        <p class="inline-block w-xl mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus autem, obcaecati possimus molestiae nihil pariatur nostrum inventore sed perferendis qui voluptas quis architecto expedita? Enim odit nostrum esse facilis rerum?</p>
+    </section>
+</main>
+
+<button class="border-2 border-green-400/40 bg-green-600/20 fixed z-40 bottom-5 right-5 rounded-full px-5 py-2 text-green-500 font-bold text-shadow-green-800 hover:bg-green-400/50 duration-150 cursor-pointer hover:text-white active:bg-green-300 active:text-green-800">
+    <span class="text-xl">&#43;</span> Salvar alterações
+</button>
